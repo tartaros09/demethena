bc5f1ac3b2ee9142b0f5356f44843973a0c077de
storm_spirit <sahil@Ivy>
2016-04-05 01:54:08 +0530

    Feature Addition: Working Calculator.
    - Created a fully working application with a separate button to
    calculate averages.
    - Added a dummy button to save results. Function might be added later.
    - Cleaned up the code to make it more streamlined.
    - Added a menubar to the application.

--- Demethena/src/gui/MainGUI.java
+++ Demethena/src/gui/MainGUI.java
@@ -8,6 +8,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -31,6 +33,7 @@ public class MainGUI
 	private JTextField globalScoreDisplayField;
 	
 	private JButton calculateButton;
+	private JButton saveResultsButton;
 	
 	// Style rows
 	private CTPRAttributeRow structureRow;
@@ -75,216 +78,286 @@ public class MainGUI
 			wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS)); {
 				
 				// Scoring panel
-				JPanel inputPanel = new JPanel();
-				inputPanel.setBorder(GUIComponentFactory.insideLineBorder);
-				inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS)); {
-					
-					// Style attributes
-					JPanel stylePanel = new JPanel();
-					stylePanel.setBorder(GUIComponentFactory.borderForStyle);
-					stylePanel.setLayout(new GridLayout(3, 1)); {
-						
-						// Structure
-						structureRow = new CTPRAttributeRow("Structure");
-						stylePanel.add(structureRow);
-						
-						// Flow of writing
-						writingFlowRow = new CTPRAttributeRow("Flow of writing");
-						stylePanel.add(writingFlowRow);
-						
-						// Language
-						languageRow = new CTPRAttributeRow("Language");
-						stylePanel.add(languageRow);
-					}
-					inputPanel.add(stylePanel);
-					
-					// Content attributes
-					JPanel contentPanel = new JPanel();
-					contentPanel.setBorder(GUIComponentFactory.borderForContent);
-					contentPanel.setLayout(new GridLayout(3, 1)); {
-						
-						// Relevance
-						relevanceRow = new CTPRAttributeRow("Relevance");
-						contentPanel.add(relevanceRow);
-						
-						// Research
-						researchRow = new CTPRAttributeRow("Research");
-						contentPanel.add(researchRow);
-						
-						// Lede and Nut-Graf
-						lngRow = new CTPRAttributeRow("Lede & Nut-Graf");
-						contentPanel.add(lngRow);
-					}
-					inputPanel.add(contentPanel);
-					
-					// Originality attributes
-					JPanel originalityPanel = new JPanel();
-					originalityPanel.setBorder(GUIComponentFactory.borderForOriginality);
-					originalityPanel.setLayout(new GridLayout(3, 1)); {
-						
-						// Making a difference
-						differenceRow = new CTPRAttributeRow("Making a difference");
-						originalityPanel.add(differenceRow);
-						
-						// Creativity
-						creativityRow = new CTPRAttributeRow("Creativity");
-						originalityPanel.add(creativityRow);
-						
-						// Why me
-						whyMeRow = new CTPRAttributeRow("Why me?");
-						originalityPanel.add(whyMeRow);
-					}
-					inputPanel.add(originalityPanel);
-					
-					// Reach attributes
-					JPanel reachPanel = new JPanel();
-					reachPanel.setBorder(GUIComponentFactory.borderForReach);
-					reachPanel.setLayout(new GridLayout(2, 1)); {
-						
-						// Target audience
-						audienceRow = new CTPRAttributeRow("Target Audience");
-						reachPanel.add(audienceRow);
-						
-						// Quality of publication
-						qualityRow = new CTPRAttributeRow("Quality of Publication");
-						reachPanel.add(qualityRow);
-					}
-					inputPanel.add(reachPanel);
-					
-					// Engagement attributes
-					JPanel engagementPanel = new JPanel();
-					engagementPanel.setBorder(GUIComponentFactory.borderForEngagement);
-					engagementPanel.setLayout(new GridLayout(2, 1)); {
-						
-						// Use of media
-						mediaRow = new CTPRAttributeRow("Use of Media");
-						engagementPanel.add(mediaRow);
-						
-						// Call for action
-						actionRow = new CTPRAttributeRow("Call for Action");
-						engagementPanel.add(actionRow);
-					}
-					inputPanel.add(engagementPanel);
-				}
+				JPanel inputPanel = buildInputPanel();
 				wrapper.add(inputPanel);
 				
 				// Button Panel
-				JPanel buttonPanel = new JPanel();
-				buttonPanel.setLayout(new GridLayout(1, 1)); {
-					calculateButton = new JButton("Calculate Scores");
-					calculateButton.addActionListener(new ActionListener() {
-						@Override
-						public void actionPerformed(ActionEvent e) {
-							styleScore = (structureRow.getRating() + writingFlowRow.getRating() + languageRow.getRating())/3;
-							styleScoreDisplayField.setText(String.valueOf(styleScore));
-							
-							contentScore = (relevanceRow.getRating() + researchRow.getRating() + lngRow.getRating())/3;
-							contentScoreDisplayField.setText(String.valueOf(contentScore));
-							
-							originalityScore = (differenceRow.getRating() + creativityRow.getRating() + whyMeRow.getRating())/3;
-							originalityScoreDisplayField.setText(String.valueOf(originalityScore));
-							
-							reachScore = (audienceRow.getRating() + qualityRow.getRating())/2;
-							reachScoreDisplayField.setText(String.valueOf(reachScore));
-							
-							engagementScore = (mediaRow.getRating() + actionRow.getRating())/2;
-							engagementScoreDisplayField.setText(String.valueOf(engagementScore));
-							
-							globalScore = (structureRow.getRating() + writingFlowRow.getRating() + languageRow.getRating() +
-									relevanceRow.getRating() + researchRow.getRating() + lngRow.getRating() +
-									differenceRow.getRating() + creativityRow.getRating() + whyMeRow.getRating() +
-									audienceRow.getRating() + qualityRow.getRating() +
-									mediaRow.getRating() + actionRow.getRating())/13;
-							globalScoreDisplayField.setText(String.valueOf(globalScore));
-						}
-					});
-					buttonPanel.add(calculateButton);
-				}
+				JPanel buttonPanel = buildButtonPanel();
 				wrapper.add(buttonPanel);
 				
 				// Results display panel
-				JPanel outputPanel = new JPanel();
-				outputPanel.setBorder(GUIComponentFactory.insideLineBorder);
-				outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS)); {
-					JPanel attributeAveragePanel = new JPanel();
-					attributeAveragePanel.setBorder(GUIComponentFactory.insideLineBorder);
-					attributeAveragePanel.setLayout(new GridLayout(5, 1)); {
-						
-						// Style output panel
-						JPanel styleOutputPanel = new JPanel();
-						styleOutputPanel.setLayout(new BoxLayout(styleOutputPanel, BoxLayout.X_AXIS)); {
-							styleOutputPanel.add(new JLabel("Style"));
-							styleScoreDisplayField = new JTextField("0.0");
-							styleScoreDisplayField.setEditable(false);
-							styleOutputPanel.add(styleScoreDisplayField);
-						}
-						attributeAveragePanel.add(styleOutputPanel);
-						
-						// Content output panel
-						JPanel contentOutputPanel = new JPanel();
-						contentOutputPanel.setLayout(new BoxLayout(contentOutputPanel, BoxLayout.X_AXIS)); {
-							contentOutputPanel.add(new JLabel("Content"));
-							contentScoreDisplayField = new JTextField("0.0");
-							contentScoreDisplayField.setEditable(false);
-							contentOutputPanel.add(contentScoreDisplayField);
-						}
-						attributeAveragePanel.add(contentOutputPanel);
-						
-						// Originality output panel
-						JPanel originalityOutputPanel = new JPanel();
-						originalityOutputPanel.setLayout(new BoxLayout(originalityOutputPanel, BoxLayout.X_AXIS)); {
-							originalityOutputPanel.add(new JLabel("Originality"));
-							originalityScoreDisplayField = new JTextField("0.0");
-							originalityScoreDisplayField.setEditable(false);
-							originalityOutputPanel.add(originalityScoreDisplayField);
-						}
-						attributeAveragePanel.add(originalityOutputPanel);
-						
-						// Reach output panel
-						JPanel reachOutputPanel = new JPanel();
-						reachOutputPanel.setLayout(new BoxLayout(reachOutputPanel, BoxLayout.X_AXIS)); {
-							reachOutputPanel.add(new JLabel("Reach"));
-							reachScoreDisplayField = new JTextField("0.0");
-							reachScoreDisplayField.setEditable(false);
-							reachOutputPanel.add(reachScoreDisplayField);
-						}
-						attributeAveragePanel.add(reachOutputPanel);
-						
-						// Engagement output panel
-						JPanel engagementOutputPanel = new JPanel();
-						engagementOutputPanel.setLayout(new BoxLayout(engagementOutputPanel, BoxLayout.X_AXIS)); {
-							engagementOutputPanel.add(new JLabel("Engagement"));
-							engagementScoreDisplayField = new JTextField("0.0");
-							engagementScoreDisplayField.setEditable(false);
-							engagementOutputPanel.add(engagementScoreDisplayField);
-						}
-						attributeAveragePanel.add(engagementOutputPanel);
-					}
-					outputPanel.add(attributeAveragePanel);
-					
-					JPanel globalAveragePanel = new JPanel();
-					globalAveragePanel.setBorder(GUIComponentFactory.insideLineBorder);
-					globalAveragePanel.setLayout(new GridLayout(1, 1)); {
-						JPanel globalOutputPanel = new JPanel();
-						globalOutputPanel.setLayout(new BoxLayout(globalOutputPanel, BoxLayout.X_AXIS)); {
-							globalOutputPanel.add(new JLabel("Total Score"));
-							globalScoreDisplayField = new JTextField("0");
-							globalScoreDisplayField.setEditable(false);
-							globalOutputPanel.add(globalScoreDisplayField);
-						}
-						globalAveragePanel.add(globalOutputPanel);
-					}
-					outputPanel.add(globalAveragePanel);
-				}
+				JPanel outputPanel = buildOutputPanel();
 				wrapper.add(outputPanel);
 			}
 			mainFrame.add(wrapper);
 		}
 		mainFrame.setResizable(false);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setJMenuBar(buildMenuBar());
 		mainFrame.pack();
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
+	}
+	
+	/******************************  ******************************/
+	
+	/**
+	 * @description build the layout for the input panel.
+	 * this function creates, initialises and add all the requisite
+	 * elements to the input panels and returns it.
+	 */
+	private JPanel buildInputPanel() {
+		JPanel ans = new JPanel();
+		ans.setBorder(GUIComponentFactory.emptyBorder);
+		ans.setLayout(new BoxLayout(ans, BoxLayout.Y_AXIS)); {
+			
+			// Style attributes
+			JPanel stylePanel = new JPanel();
+			stylePanel.setBorder(GUIComponentFactory.borderForStyle);
+			stylePanel.setLayout(new GridLayout(3, 1)); {
+				
+				// Structure
+				structureRow = new CTPRAttributeRow("Structure");
+				stylePanel.add(structureRow);
+				
+				// Flow of writing
+				writingFlowRow = new CTPRAttributeRow("Flow of writing");
+				stylePanel.add(writingFlowRow);
+				
+				// Language
+				languageRow = new CTPRAttributeRow("Language");
+				stylePanel.add(languageRow);
+			}
+			ans.add(stylePanel);
+			
+			// Content attributes
+			JPanel contentPanel = new JPanel();
+			contentPanel.setBorder(GUIComponentFactory.borderForContent);
+			contentPanel.setLayout(new GridLayout(3, 1)); {
+				
+				// Relevance
+				relevanceRow = new CTPRAttributeRow("Relevance");
+				contentPanel.add(relevanceRow);
+				
+				// Research
+				researchRow = new CTPRAttributeRow("Research");
+				contentPanel.add(researchRow);
+				
+				// Lede and Nut-Graf
+				lngRow = new CTPRAttributeRow("Lede & Nut-Graf");
+				contentPanel.add(lngRow);
+			}
+			ans.add(contentPanel);
+			
+			// Originality attributes
+			JPanel originalityPanel = new JPanel();
+			originalityPanel.setBorder(GUIComponentFactory.borderForOriginality);
+			originalityPanel.setLayout(new GridLayout(3, 1)); {
+				
+				// Making a difference
+				differenceRow = new CTPRAttributeRow("Making a difference");
+				originalityPanel.add(differenceRow);
+				
+				// Creativity
+				creativityRow = new CTPRAttributeRow("Creativity");
+				originalityPanel.add(creativityRow);
+				
+				// Why me
+				whyMeRow = new CTPRAttributeRow("Why me?");
+				originalityPanel.add(whyMeRow);
+			}
+			ans.add(originalityPanel);
+			
+			// Reach attributes
+			JPanel reachPanel = new JPanel();
+			reachPanel.setBorder(GUIComponentFactory.borderForReach);
+			reachPanel.setLayout(new GridLayout(2, 1)); {
+				
+				// Target audience
+				audienceRow = new CTPRAttributeRow("Target Audience");
+				reachPanel.add(audienceRow);
+				
+				// Quality of publication
+				qualityRow = new CTPRAttributeRow("Quality of Publication");
+				reachPanel.add(qualityRow);
+			}
+			ans.add(reachPanel);
+			
+			// Engagement attributes
+			JPanel engagementPanel = new JPanel();
+			engagementPanel.setBorder(GUIComponentFactory.borderForEngagement);
+			engagementPanel.setLayout(new GridLayout(2, 1)); {
+				
+				// Use of media
+				mediaRow = new CTPRAttributeRow("Use of Media");
+				engagementPanel.add(mediaRow);
+				
+				// Call for action
+				actionRow = new CTPRAttributeRow("Call for Action");
+				engagementPanel.add(actionRow);
+			}
+			ans.add(engagementPanel);
+		}
+		return ans;
+	}
+	
+	/******************************  ******************************/
+	
+	/**
+	 * @description build the layout for the button panel.
+	 * this function creates, initialises and add all the requisite
+	 * elements to the button panels and returns it.
+	 */
+	private JPanel buildButtonPanel() {
+		JPanel ans = new JPanel();
+		ans.setLayout(new GridLayout(1, 2)); {
+			calculateButton = new JButton("Calculate Scores");
+			calculateButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					styleScore = (structureRow.getRating() + writingFlowRow.getRating() + languageRow.getRating())/3;
+					styleScoreDisplayField.setText(String.valueOf(styleScore));
+					
+					contentScore = (relevanceRow.getRating() + researchRow.getRating() + lngRow.getRating())/3;
+					contentScoreDisplayField.setText(String.valueOf(contentScore));
+					
+					originalityScore = (differenceRow.getRating() + creativityRow.getRating() + whyMeRow.getRating())/3;
+					originalityScoreDisplayField.setText(String.valueOf(originalityScore));
+					
+					reachScore = (audienceRow.getRating() + qualityRow.getRating())/2;
+					reachScoreDisplayField.setText(String.valueOf(reachScore));
+					
+					engagementScore = (mediaRow.getRating() + actionRow.getRating())/2;
+					engagementScoreDisplayField.setText(String.valueOf(engagementScore));
+					
+					globalScore = (structureRow.getRating() + writingFlowRow.getRating() + languageRow.getRating() +
+							relevanceRow.getRating() + researchRow.getRating() + lngRow.getRating() +
+							differenceRow.getRating() + creativityRow.getRating() + whyMeRow.getRating() +
+							audienceRow.getRating() + qualityRow.getRating() +
+							mediaRow.getRating() + actionRow.getRating())/13;
+					globalScoreDisplayField.setText(String.valueOf(globalScore));
+				}
+			});
+			ans.add(calculateButton);
+			
+			saveResultsButton = new JButton("Save Results");
+			saveResultsButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					
+				}
+			});
+			ans.add(saveResultsButton);
+		}
+		return ans;
+	}
+	
+	/******************************  ******************************/
+	
+	/**
+	 * @description build the layout for the output panel.
+	 * this function creates, initialises and add all the requisite
+	 * elements to the output panels and returns it.
+	 */
+	private JPanel buildOutputPanel() {
+		JPanel ans = new JPanel();
+		ans.setBorder(GUIComponentFactory.emptyBorder);
+		ans.setLayout(new BoxLayout(ans, BoxLayout.Y_AXIS)); {
+			JPanel attributeAveragePanel = new JPanel();
+			attributeAveragePanel.setBorder(GUIComponentFactory.emptyBorder);
+			attributeAveragePanel.setLayout(new GridLayout(5, 1)); {
+				
+				// Style output panel
+				JPanel styleOutputPanel = new JPanel();
+				styleOutputPanel.setLayout(new BoxLayout(styleOutputPanel, BoxLayout.X_AXIS)); {
+					styleOutputPanel.add(new JLabel("Style"));
+					styleScoreDisplayField = new JTextField("0.0");
+					styleScoreDisplayField.setEditable(false);
+					styleOutputPanel.add(styleScoreDisplayField);
+				}
+				attributeAveragePanel.add(styleOutputPanel);
+				
+				// Content output panel
+				JPanel contentOutputPanel = new JPanel();
+				contentOutputPanel.setLayout(new BoxLayout(contentOutputPanel, BoxLayout.X_AXIS)); {
+					contentOutputPanel.add(new JLabel("Content"));
+					contentScoreDisplayField = new JTextField("0.0");
+					contentScoreDisplayField.setEditable(false);
+					contentOutputPanel.add(contentScoreDisplayField);
+				}
+				attributeAveragePanel.add(contentOutputPanel);
+				
+				// Originality output panel
+				JPanel originalityOutputPanel = new JPanel();
+				originalityOutputPanel.setLayout(new BoxLayout(originalityOutputPanel, BoxLayout.X_AXIS)); {
+					originalityOutputPanel.add(new JLabel("Originality"));
+					originalityScoreDisplayField = new JTextField("0.0");
+					originalityScoreDisplayField.setEditable(false);
+					originalityOutputPanel.add(originalityScoreDisplayField);
+				}
+				attributeAveragePanel.add(originalityOutputPanel);
+				
+				// Reach output panel
+				JPanel reachOutputPanel = new JPanel();
+				reachOutputPanel.setLayout(new BoxLayout(reachOutputPanel, BoxLayout.X_AXIS)); {
+					reachOutputPanel.add(new JLabel("Reach"));
+					reachScoreDisplayField = new JTextField("0.0");
+					reachScoreDisplayField.setEditable(false);
+					reachOutputPanel.add(reachScoreDisplayField);
+				}
+				attributeAveragePanel.add(reachOutputPanel);
+				
+				// Engagement output panel
+				JPanel engagementOutputPanel = new JPanel();
+				engagementOutputPanel.setLayout(new BoxLayout(engagementOutputPanel, BoxLayout.X_AXIS)); {
+					engagementOutputPanel.add(new JLabel("Engagement"));
+					engagementScoreDisplayField = new JTextField("0.0");
+					engagementScoreDisplayField.setEditable(false);
+					engagementOutputPanel.add(engagementScoreDisplayField);
+				}
+				attributeAveragePanel.add(engagementOutputPanel);
+			}
+			ans.add(attributeAveragePanel);
+			
+			// Global output panel
+			JPanel globalAveragePanel = new JPanel();
+			globalAveragePanel.setBorder(GUIComponentFactory.emptyBorder);
+			globalAveragePanel.setLayout(new GridLayout(1, 1)); {
+				JPanel globalOutputPanel = new JPanel();
+				globalOutputPanel.setLayout(new BoxLayout(globalOutputPanel, BoxLayout.X_AXIS)); {
+					globalOutputPanel.add(new JLabel("Total Score"));
+					globalScoreDisplayField = new JTextField("0");
+					globalScoreDisplayField.setEditable(false);
+					globalOutputPanel.add(globalScoreDisplayField);
+				}
+				globalAveragePanel.add(globalOutputPanel);
+			}
+			ans.add(globalAveragePanel);
+		}
+		return ans;
+	}
+	
+	/******************************  ******************************/
+	
+	/**
+	 * @description build the menu bar for the application.
+	 */
+	private JMenuBar buildMenuBar() {
+		JMenuBar ans = new JMenuBar(); {
+			
+			// File menu
+			JMenu fileMenu = new JMenu("File"); {
+				
+			}
+			ans.add(fileMenu);
+			
+			// Help menu
+			JMenu helpMenu = new JMenu("Help"); {
+				
+			}
+			ans.add(helpMenu);
+		}
+		return ans;
 	}
 	
 	/******************************  ******************************/
